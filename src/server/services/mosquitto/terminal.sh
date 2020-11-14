d2ce9a6a378d39b6081bc4e84575ec86dcf3fc33
Joris Navarro <joris.navarro@braincities.co>
2020-11-15 00:27:11 +0100

    add mosquitto mqtt broker container

---
+++ src/server/services/mosquitto/terminal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "you are about to enter the shell for mosquitto"
+echo "the documentation explains how to secure mosquitto with a username and password."
+
+docker exec -it mosquitto sh
